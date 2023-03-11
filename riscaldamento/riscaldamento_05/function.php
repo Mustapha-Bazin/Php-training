10ecc11ec8fad1a8ea9c7a25e7e8d4859d152e52
Mustapha Bazin <mustapha.bazin@edu.unito.it>
2023-03-11 13:01:00 +0100

    MDF codice ol ul

--- riscaldamento/riscaldamento_05/function.php
+++ riscaldamento/riscaldamento_05/function.php
@@ -1,11 +1,17 @@
 <?php
 
-function list1($array){
-    echo "<ul>";
+function list1($array,$type=0){
+    if($type==0){
+        $html="ul";
+    }elseif($type==1){
+        $html="ol";
+        sort($array,SORT_STRING);
+    }
+    echo "<".$html.">";
     foreach($array as $ar){
         echo "<li>$ar</li>";
     }
-    echo "</ul>";
+    echo "</".$html.">";
 }
 
 ?>
